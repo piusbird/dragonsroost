a403b9b7f02c0742b66cf7f18469f1b57aa8c34e
Matt Arnold <matt@thegnuguru.org>
2023-02-05 06:05:29 -0500

    fat fingers

--- prodsetup.go
+++ prodsetup.go
@@ -3,8 +3,8 @@
 package main
 
 import "net/http"
-func setupDatabase(w, http.ResponseWriter, r *http.Request) {
+
+func setupDatabase(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Not in production bro!", http.StatusForbidden)
 	return
-} 
-
+}
