ae7f849ccf6829fcd75624d2e45d4aca0cb291f8
Matt Arnold <matt@thegnuguru.org>
2023-02-10 05:28:28 -0500

    more activitypub gore

--- activitypub.go
+++ activitypub.go
@@ -33,14 +33,12 @@ var user = `{
 	"summary": "Server Server Server",
 	"manuallyApprovesFollowers": true,
     "discoverable": true,
-    "published": "1984-04-20T00:00:00Z",
+    "published": "2019-04-20T00:00:00Z",
 
 	"inbox": "https://treefort.piusbird.space/inbox",
-	"publicKey": {
-	  "id": "https://treefort.piusbird.space/u/piusbird/key",
-	  "owner": "https://treefort.piusbird.space/u/piusbird",
-	  "publickeyPem": "-----BEGIN PUBLIC KEY-----\\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAozrjVu0XYfjYHyk/eoX6\\nhHfTu+qsq+T299wqTJ/vp7l3pAVttUR6eNS5kZNs4Ugr+MheJf/GS2odH8pDSkRt\\naRCq4Cs0uMHBzN+tLaE1lWR6O+fZX6nyNXwax+XRRuAgyr1ciazWGs+1r50JnbJH\\nYI9tofeiq5UaRUyNP2SLvNmDKsXcdL2qw2UQMfJQj/pKAFNnz31xrFvAAQ3S24aC\\nO7bdZ9fIMPfgwudSkga8Hs0ACYW+AXSSDMnwnJ631hSKicU4QAKCbn+cziGR5ZHY\\nbX3xe96NGXJAcA7y9AcQG2REcUvUeXp6XW7KoaBjNN47d/h+F6DmQ/FJPvQUI9bW\\n0QIDAQAB\\n-----END PUBLIC KEY-----\\n"
-	}
+	"outbox": "https://treefort.piusbird.space/outbox",
+	
+	
   }
   `
 
